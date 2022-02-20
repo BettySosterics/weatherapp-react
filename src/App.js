50b914d29bbbdd7b9943b9b2ebd087c0714a3cdc
BettySosterics <betty.sosterics@gmail.com>
2022-02-20 16:52:45 +0100

    added html to react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import Weather from "./Weather";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import { Audio } from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello react</h1>
         <Weather />
-        <Audio height="100" width="100" color="grey" ariaLabel="loading" />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,68 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
 export default function Weather() {
-  function handleSubmit(response) {
-    alert(`The weather in Paris is ${response.data.main.temp}°C`);
-  }
+  return (
+    <div className="container">
+      <div className="card currentCity">
+        <form className="form-inline" id="city-form">
+          <input
+            type="text"
+            placeholder="Enter a city"
+            className="form-control form-control-sm"
+            id="city-input"
+            autofocus
+            autocomplete="off"
+          />
+          <div className="col searchButton">
+            <button type="submit" className="btn btn-light btn-sm">
+              Search
+            </button>
+          </div>
+        </form>
+        <div className="row currentCityData">
+          <div className="col-6" id="current-city">
+            Vienna
+          </div>
+          <div className="col-2"></div>
+          <span id="current-temperature">21</span>
+          °C
+        </div>
 
-  let apiKey = "094780c710fa4efd669f0df8c3991927";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleSubmit);
+        <div className="row">
+          <div className="col-8">
+            <p className="moreInfo">
+              <span id="descr"></span>
+              <br />
+              humidity: <span id="humidity">12</span>%
+              <br />
+              wind: <span id="wind"> 55</span>km/h
+            </p>
+          </div>
+          <div className="col-4">
+            <img
+              src="http://openweathermap.org/img/wn/10d@2x.png"
+              alt=""
+              id="currentWeatherLogo"
+              width="90"
+            />
+          </div>
+        </div>
+        <br />
+        <div className="weather-forecast" id="forecast"></div>
+      </div>
+      <br />
 
-  return <h1>weather in chicago is 19C</h1>;
+      <p>
+        <a
+          href="https://github.com/BettySosterics/weatherapp-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Betty Sosterics
+      </p>
+    </div>
+  );
 }
