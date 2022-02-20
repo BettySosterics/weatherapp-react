fcc5f3d801e70e6ba07c20b2c2d6d8826cdedf3b
BettySosterics <betty.sosterics@gmail.com>
2022-02-20 09:47:49 +0100

    created react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Audio } from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello react</h1>
+        <Weather />
+        <Audio height="100" width="100" color="grey" ariaLabel="loading" />
       </header>
     </div>
   );
