44358862087d715cf1823d95d445fcd54dc32b1e
BettySosterics <betty.sosterics@gmail.com>
2022-02-27 18:02:43 +0100

    add base html codes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>React Weather App</h1>
-          <p>
-            Coded by Betty Sosterics opensourced on{" "}
-            <a
-              href="https://github.com/BettySosterics/weatherapp-react"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-            .
-          </p>
-        </header>
+        <Weather />
+        <footer>
+          Coded by Betty Sosterics opensourced on{" "}
+          <a
+            href="https://github.com/BettySosterics/weatherapp-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -3,66 +3,26 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="container">
-      <div className="card currentCity">
-        <form className="form-inline" id="city-form">
-          <input
-            type="text"
-            placeholder="Enter a city"
-            className="form-control form-control-sm"
-            id="city-input"
-            autofocus
-            autocomplete="off"
-          />
-          <div className="col searchButton">
-            <button type="submit" className="btn btn-light btn-sm">
-              Search
-            </button>
-          </div>
-        </form>
-        <div className="row currentCityData">
-          <div className="col-6" id="current-city">
-            Vienna
-          </div>
-          <div className="col-2"></div>
-          <span id="current-temperature">21</span>
-          °C
-        </div>
-
+    <div className="Weather">
+      <form>
         <div className="row">
-          <div className="col-8">
-            <p className="moreInfo">
-              <span id="descr"></span>
-              <br />
-              humidity: <span id="humidity">12</span>%
-              <br />
-              wind: <span id="wind"> 55</span>km/h
-            </p>
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+            />
           </div>
-          <div className="col-4">
-            <img
-              src="http://openweathermap.org/img/wn/10d@2x.png"
-              alt=""
-              id="currentWeatherLogo"
-              width="90"
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
             />
           </div>
         </div>
-        <br />
-        <div className="weather-forecast" id="forecast"></div>
-      </div>
-      <br />
-
-      <p>
-        <a
-          href="https://github.com/BettySosterics/weatherapp-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Betty Sosterics
-      </p>
+      </form>
     </div>
   );
 }
