32fafe17b0ee0bdc6d2c3b4fb4f82a4f417f007c
BettySosterics <betty.sosterics@gmail.com>
2022-02-27 17:11:15 +0100

    added npm bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-        <p>
-          Coded by Betty Sosterics opensourced on{" "}
-          <a
-            href="https://github.com/BettySosterics/weatherapp-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-          .
-        </p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>React Weather App</h1>
+          <p>
+            Coded by Betty Sosterics opensourced on{" "}
+            <a
+              href="https://github.com/BettySosterics/weatherapp-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+        </header>
+      </div>
     </div>
   );
 }
