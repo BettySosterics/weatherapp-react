871beebe2dd46af0769ef00d14c8703a5515cf45
BettySosterics <betty.sosterics@gmail.com>
2022-03-02 21:03:22 +0100

    updated html

--- src/Weather.js
+++ src/Weather.js
@@ -6,23 +6,39 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-9">
+          <div className="col-8">
             <input
               type="search"
-              placeholder="Enter a city.."
+              placeholder="Search for a city"
               className="form-control"
-              autoFocus="on"
             />
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+          <div className="col-4">
+            <input type="submit" value="Search" className="btn btn-info" />
           </div>
         </div>
       </form>
+      <h1>Vienna</h1>
+      <ul>
+        <li>Thursday 12:00</li>
+        <li>Rainy day</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+            alt="Rainy day"
+          />
+          12°c
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 12%</li>
+            <li>Humidity: 8%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
