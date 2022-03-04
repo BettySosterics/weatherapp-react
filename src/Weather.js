51be6fd7aaf898925edb0c3f47d7917b5e884263
BettySosterics <betty.sosterics@gmail.com>
2022-03-04 17:46:34 +0100

    added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: "Thursday 13:00",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Search for a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-4">
+              <input type="submit" value="Search" className="btn btn-info" />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-8">
-            <input
-              type="search"
-              placeholder="Search for a city"
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-4">
-            <input type="submit" value="Search" className="btn btn-info" />
-          </div>
-        </div>
-      </form>
-      <h1>Vienna</h1>
-      <ul>
-        <li>Thursday 12:00</li>
-        <li>Rainy day</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-            alt="Rainy day"
-          />
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
 
-          <span className="temperature">12</span>
-          <span className="unit">°C|°F</span>
-        </div>
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C|°F</span>
+          </div>
 
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 12%</li>
-            <li>Humidity: 8%</li>
-            <li>Wind: 5 km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 12%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "9d2606ef8c586cbebc46ab5a29408811";
+
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
