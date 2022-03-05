7f5413f498e5aaa33bf974eb44351f3f7bffe667
BettySosterics <betty.sosterics@gmail.com>
2022-03-05 18:41:48 +0100

    added https

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
   function search() {
     const apiKey = "9d2606ef8c586cbebc46ab5a29408811";
 
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
